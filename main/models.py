99b131126a545e05c6f968bbaff8da3c38990886
Роман Рыбкин < >
2017-02-06 17:49:46 +0300

    add files

--- main/models.py
+++ main/models.py
@@ -5,6 +5,26 @@ TYPE_USER = {
 	(1, 'Admin'),
 	(2, 'User')
 }
+QUESTION_1 = {
+	(1, 'Я смелый человек'),
+	(2, 'Я счастливый человек'),
+	(3, 'Я несчастен'),
+}
+QUESTION_2 = {
+	(1, 'Я успешный человек'),
+	(2, 'Я счастливый человек'),
+	(3, 'Я несчастен'),
+}
+QUESTION_3 = {
+	(1, 'Я хорошо вижу'),
+	(2, 'Я счастливый человек'),
+	(3, 'Я не вижу'),
+}
+QUESTION_TITLE = {
+	(1, 'Тест на смелось'),
+	(2, 'Тест на успешность'),
+	(3, 'Тест на зоркость'),
+}
 
 
 class User(models.Model):
@@ -18,6 +38,22 @@ class User(models.Model):
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
+	published_date = models.DateTimeField(
+		blank=True, null=True)
+	created_date = models.DateTimeField(
+		default=timezone.now)
+
+	def __str__(self):
+		return self.title
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+
+class Test(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length= 200)
 	published_date = models.DateTimeField(
 		blank=True, null=True)
 	created_date = models.DateTimeField(
